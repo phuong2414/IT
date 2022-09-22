0aa342c56183fed7bf2ab547b7dbdee21b6fc9f6
phuong2414 <98210417+phuong2414@users.noreply.github.com>
2022-09-22 22:47:31 +0700

    fit js

--- Mis1+2/test/js/script.js
+++ Mis1+2/test/js/script.js
@@ -1,9 +1,11 @@
 var swiper = new Swiper(".slide-content", {
-  slidesPerView: 1,
+  slidesPerView: 3,
   spaceBetween: 25,
-  loopFillGroupWithBlank: true,
+  loop: true,
+  fade: true,
+  //loopFillGroupWithBlank: true,
   centeredSlides: false,
-  slidesPerGroupSkip: 1,
+  //slidesPerGroupSkip: 1,
   grabCursor: true,
   keyboard: {
     enabled: true,
