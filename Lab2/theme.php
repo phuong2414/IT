2e639acf9609b7d03bbc8ad1ea798024c7f5e3e1
phuong2414 <98210417+phuong2414@users.noreply.github.com>
2022-10-07 10:37:15 +0700

    footer

---
+++ Lab2/theme.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+    <!--Footer-->
+    <?php include("./footer.html");?>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+</body>
+</html>
